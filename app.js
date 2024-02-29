9807d279b0f0c7c32ea9307cf45e223002708317
Zuhaibsha <zuhaibshah709@gmail.com>
2024-02-29 19:18:09 +0500

    CLASS 1

---
+++ app.js
@@ -0,0 +1,178 @@
+
+//chapter 3
+
+// Question 1
+// var age="Im 15 year old";
+
+// alert(age);
+
+
+
+// // Question 3
+//  var birth="my birth year is 2003";
+
+// document.write(birth);
+  
+
+// Question 4
+// document.write("John Doe ordered 5-T shirts on XYZ clothing store"); 
+
+
+
+//    chapter 2
+
+// Question 1
+
+// var username= zohaib;
+ 
+// //Question 2
+//  var myName="zohaib Ali Shah";
+
+// Question 3
+
+// var message="Hello world"
+
+// alert(message)
+
+// var name="zohaib";
+// var age=" 20 year old";
+// var certification="certified web and app developer"
+
+
+
+// alert(name)
+// alert(age)
+// alert( certification
+
+// )
+     
+// Question 5
+// alert("pizza\n pizz\n piz\n pi\n p")
+
+// Question 6
+// var email="example@gmail.com";
+
+// alert("  my email address "  + email    )
+
+// Question 7
+
+// var books="A smarter way to learn java script";
+
+// alert("I am trying to learn from the book  "   +books);
+
+
+// Question 8
+
+// document.write("yah i can write HTML content through java script")
+// // //chapter 1
+
+//Question 1
+
+// alert("Welcome to our website")
+
+//Question 2
+
+// var error ="Error! Enter a valid password";
+
+// alert(error)
+ 
+// Question 3
+
+// alert("Welcome to JS land.....\n Happy coding")
+
+
+// Question 4
+// alert("Welcome to JS land");
+// alert("Happy coding");
+
+
+
+// Question 5
+// console.log("Hello ....i can run my web browser console");
+
+
+//Question 6 and 7
+
+
+/* <head>
+<script>
+function showAlert() {
+  alert("This is an alert message!");
+}
+</script>
+
+
+
+</head> */
+
+
+/* <body>
+
+<script>
+  function showAlert() {
+    alert("This is an alert message!");
+  }
+</script>
+
+<button onclick="showAlert()">Show Alert</button>
+
+</body> */
+
+
+// <body>
+
+// <button onclick="showAlert()">Show Alert</button>
+
+// <script>
+//   function showAlert() {
+//     alert("This is an alert message!");
+//   }
+// </script>
+
+// </body>
+
+
+
+{/* <body>
+
+<button onclick="showAlert()">Show Alert</button>
+
+</body>
+
+<script>
+  function showAlert() {
+    alert("This is an alert message!");
+  }
+</script> */}
+
+
+ssk BKXHkbkLJGK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
